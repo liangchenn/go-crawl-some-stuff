4ed7faaf5d63a9de7670ae393791b909d7a8b4b3
liangchenn <r08323022@ntu.edu.tw>
2022-06-05 14:46:21 +0000

    add goroutine for index page parser

--- main.go
+++ main.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"strings"
+	"log"
+	"sync"
 
 	"github.com/liangchenn/go-crawl-some-stuff/ptt"
 )
 
 func main() {
 
-	// create index page url
-	board := strings.ToUpper("fapl")
+	board := "Python"
 	data := ptt.GetLinksByBoard(board)
 	// fmt.Println(data)
-	file, _ := json.MarshalIndent(data, "", "	")
-	_ = ioutil.WriteFile("page.json", file, 0644)
+	// file, _ := json.MarshalIndent(data, "", "	")
+	// _ = ioutil.WriteFile(fmt.Sprintf("%s-index.json", board), file, 0644)
+
+	articles := make([]*ptt.Article, len(data))
+
+	// using goroutine
+	var wg sync.WaitGroup
+
+	for i, post := range data {
+		wg.Add(1)
+		log.Printf("[%d] parsing article: %s\n", i, post.Link)
+
+		// a := ptt.ParsePostPage(&post)
+		// articles[i] = a
+
+		go func(p *ptt.Post, i int, wg *sync.WaitGroup) {
+			defer wg.Done()
+			a := ptt.ParsePostPage(p)
+			articles[i] = a
+		}(&post, i, &wg)
+
+		wg.Wait()
+
+	}
+
+	file, _ := json.MarshalIndent(articles, "", "	")
+	_ = ioutil.WriteFile(fmt.Sprintf("data/%s-result.json", board), file, 0644)
+
 }
